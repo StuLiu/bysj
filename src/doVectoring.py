851903a9ccea4925ad47a4fb3634155adb63bc0c
liuwang20144623 <1183862787@qq.com>
2018-04-10 22:51:57 +0800

    新增脚本——将标注好的评论向量化并存入数据库

---
+++ src/doVectoring.py
@@ -0,0 +1,34 @@
+"""
+============================================
+    将标注好的评论文本向量化，并存入数据库
+ 1. 从signedcomments表中查询标注好的评论
+ 2. 向量化评论
+ 3. 将向量化的评论插入vectorizedcomments表中
+============================================
+"""
+print(__doc__)
+
+from database.vectorized_comments_dbHandler import VectorizedCommentsDbHandler
+from database.signed_comments_dbHandler import SignedCommentsDbHandler
+from scikitLearn.vectorizer import Vectorizer
+
+import time
+if __name__ == "__main__":
+    fr = time.time()
+    # 已标注评论数据库表的操作对象
+    signedCommentsHandler = SignedCommentsDbHandler()
+    # 向量化评论数据库表的操作对象
+    vectorizedCommentsDbHandler = VectorizedCommentsDbHandler()
+    # 用于向量化的类
+    vectorizer = Vectorizer()
+    # 标注好的评论元组
+    signedComments = signedCommentsHandler.queryAll()
+    for signedComment in signedComments:
+        vectorizedComment = vectorizer.vectoring(signedComment[1],
+                                                 signedComment[2],
+                                                 signedComment[3],
+                                                 signedComment[6])
+        print(signedComment,'\n',vectorizedComment)
+        vectorizedCommentsDbHandler.insertVectorizedComment(vectorizedComment)
+    to = time.time()
+    print("用时:%f"%(to-fr))
