891057559d766ba5ed3bb18a133836aa2e906cf2
liuwang20144623 <1183862787@qq.com>
2018-04-10 23:35:52 +0800

    修改数据库配置文件加载路径

--- src/database/dataBaseArgs.py
+++ src/database/dataBaseArgs.py
@@ -1,23 +1,32 @@
 
 import xml.dom.minidom as xmlDom
+
+import config
+
+
 class DataBaseArgs(object):
-    DOMTree = xmlDom.parse("mysqlArgs.xml")
+    DOMTree = xmlDom.parse(config.MYSQLARGS_PATH)
     mysql = DOMTree.documentElement.getElementsByTagName("mysql")[0]
+    url = mysql.getElementsByTagName('url')[0].childNodes[0].data
     userName = mysql.getElementsByTagName('userName')[0].childNodes[0].data
     password = mysql.getElementsByTagName('password')[0].childNodes[0].data
     database = mysql.getElementsByTagName('database')[0].childNodes[0].data
 
     @classmethod
-    def getUserName(self):
-        return self.userName
+    def getUserName(cls):
+        return cls.userName
 
     @classmethod
-    def getPassWord(self):
-        return self.password
+    def getPassWord(cls):
+        return cls.password
 
     @classmethod
-    def getDataBase(self):
-        return self.database
+    def getDataBase(cls):
+        return cls.database
+
+    @classmethod
+    def getUrl(cls):
+        return cls.url
 
 if __name__ == '__main__':
     print(DataBaseArgs.getUserName())
