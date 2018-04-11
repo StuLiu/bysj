1fbdc99064d3fc44b13c2b53da1c9cd90e5a92d5
liuwang20144623 <1183862787@qq.com>
2018-04-11 22:06:03 +0800

    增加单元测试代码

--- src/database/dataBaseArgs.py
+++ src/database/dataBaseArgs.py
@@ -5,28 +5,31 @@ import config
 
 
 class DataBaseArgs(object):
-    DOMTree = xmlDom.parse(config.MYSQLARGS_PATH)
-    mysql = DOMTree.documentElement.getElementsByTagName("mysql")[0]
-    url = mysql.getElementsByTagName('url')[0].childNodes[0].data
-    userName = mysql.getElementsByTagName('userName')[0].childNodes[0].data
-    password = mysql.getElementsByTagName('password')[0].childNodes[0].data
-    database = mysql.getElementsByTagName('database')[0].childNodes[0].data
+    __DOMTree = xmlDom.parse(config.MYSQLARGS_PATH)
+    __mysql = __DOMTree.documentElement.getElementsByTagName("mysql")[0]
+    __url = __mysql.getElementsByTagName('url')[0].childNodes[0].data
+    __userName = __mysql.getElementsByTagName('userName')[0].childNodes[0].data
+    __password = __mysql.getElementsByTagName('password')[0].childNodes[0].data
+    __database = __mysql.getElementsByTagName('database')[0].childNodes[0].data
 
     @classmethod
     def getUserName(cls):
-        return cls.userName
+        return cls.__userName
 
     @classmethod
     def getPassWord(cls):
-        return cls.password
+        return cls.__password
 
     @classmethod
     def getDataBase(cls):
-        return cls.database
+        return cls.__database
 
     @classmethod
     def getUrl(cls):
-        return cls.url
+        return cls.__url
 
 if __name__ == '__main__':
     print(DataBaseArgs.getUserName())
+    print(DataBaseArgs.getPassWord())
+    print(DataBaseArgs.getDataBase())
+    print(DataBaseArgs.getUrl())
