0c0b1068aff0563875435a853877f2a712898c53
liuwang20144623 <1183862787@qq.com>
2018-05-08 12:50:13 +0800

    修改函数参数和返回值

--- src/textProcessing/fit_and_evaluate.py
+++ src/textProcessing/fit_and_evaluate.py
@@ -1,19 +1,26 @@
 from sklearn.metrics import classification_report
 from sklearn.cross_validation import cross_val_score
 import numpy as np
-def fit_evalueate(classifiers, X, y):
+def fit_evalueate(classifier, X, y):
     """
-    :param classifiers:字典类型{"classifier name':classifier object,...}
-    :param data:
-    :todo:用不同的机器学习分类器对传入的data训练数据进行训练，然后用data测试数据进行评估
-    :return: None
+    :param classifier:classifier object
+    :param X:特征向量集合（ndarray）
+    :param y:X特征向量对应的所属类型集合（ndarray）
+    :todo:用机器学习分类器classifier进行训练，打印评分
+    :return: 交叉验证得分均值
     """
+    # 训练样本和测试样本的总数据量
     dataLen = len(y)
-    for key in classifiers.keys():
-        print('classifier:', key)
-        classifiers[key].fit(X[:int(0.9*dataLen)], y[:int(0.9*dataLen)])
-        print(classification_report(y[int(0.9*dataLen):], classifiers[key].predict(X[int(0.9*dataLen):])))
-        cross_val_score_list = cross_val_score(classifiers[key], X, y, n_jobs=1, cv=5)
-        print('cross_val_score:',cross_val_score_list)
-        print("平均得分:",np.mean(cross_val_score_list))
-        print('-'*80,end='\n')
+    print("样本量:",dataLen)
+    try:
+        classifier.fit(X[:int(0.8*dataLen)], y[:int(0.8*dataLen)])
+    except Warning as e:
+        print(e)
+    print(classification_report(y[int(0.8*dataLen):], classifier.predict(X[int(0.8*dataLen):])))
+
+    cross_val_score_list = cross_val_score(classifier, X, y, n_jobs=1, cv=3)
+    print('cross_val_score:',cross_val_score_list)
+    print("平均得分:",np.mean(cross_val_score_list))
+    print('-'*80,end='\n')
+
+    return np.mean(cross_val_score_list)
